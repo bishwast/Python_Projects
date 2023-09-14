2d56e4ef8d320877985afe8a02893c5e64736c46
bishwast77 <bishwast77@gmail.com>
2023-09-14 16:44:47 -0600

    update2

---
+++ sb_calculator.py
@@ -0,0 +1,41 @@
+print("Sunil's Calculator App!")
+
+"""
+Defining set of functions that will implement the calculator operations below:
+1. Addition
+2. Subtraction
+3. Multiplication
+4. Division
+
+- Functions will take 2 parameters and return another number. 
+"""
+
+def addition(x,y):
+    """
+    Adds two numbers from x and y variables.
+    :returns x+y
+    """
+    return x+y
+
+def subtraction(x,y):
+    """
+    Subtracts two numbers from x and y variables.
+    :return: x-y
+    """
+    return x-y
+
+def multiplication(x,y):
+    """
+    Multiplies numbers from variables x and y.
+    :return: x*y
+    """
+    return x*y
+
+def division(x,y):
+    """
+    Dividies x from y
+    :param x:
+    :param y:
+    :return: quotient
+    """
+    return x/y
