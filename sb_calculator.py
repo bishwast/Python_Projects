4617bcf83cafc17d4466afaff4580ee30904a9b0
bishwast77 <bishwast77@gmail.com>
2023-09-14 22:46:22 -0600

    update6

--- sb_calculator.py
+++ sb_calculator.py
@@ -40,6 +40,21 @@ def division(x,y):
     """
     return x/y
 
+def modulus(x,y):
+    """
+    Calculates the modulus of x divided by y.
+    :return: x % y
+    """
+    return x % y
+
+def power_of(x,y):
+    """
+    Calculates the power of x for y value.
+    :return: x**y
+    """
+    return x**y
+
+
 # Verification - Reject any input until user input is either y or n.
 def check_if_the_user_has_finished():
     """
@@ -52,16 +67,18 @@ def check_if_the_user_has_finished():
     user_input_accepted = False
 
     while not user_input_accepted:
-        user_input = input("Do you want to continue? (y/n): ")
+        user_input = input("Do you want to finish? (y/n): ")
         if user_input == 'y':
             user_input_accepted = True
         elif user_input == 'n':
-            user_input_accepted = False
+            ok_to_finish = False
+            user_input_accepted = True
         else:
             print("Invalid Entry! Please enter 'y' or 'n'.")
     # User is ok to finish / True if input is 'y' by default. If enters y python understands the boolean logic is False.
     return ok_to_finish
 
+## Selecting the Operation
 def get_operation_choice():
     """
     Get a valid input from the user for operations to process.
@@ -74,33 +91,70 @@ def get_operation_choice():
         print("Following are the operator to choose from:")
         print("\t1. Add")
         print("\t2. Subtract")
-        print("\t3.Multiply")
-        print("\t4.Divide")
+        print("\t3. Multiply")
+        print("\t4. Divide")
+        print("\t5. Modulus")
+        print("\t6. Power Of")
         print("--"*50)
-        user_selection = input("Please make a selection from 1 to 4: ")
-        if user_selection in ('1', '2','3','4'):
+        user_selection = input("Please make a selection from 1 to 6: ")
+        if user_selection in ('1', '2','3','4','5','6'):
             input_ok = True
         else:
-            print("Invalid Selection! Please select from 1 to 4 only.")
+            print("Invalid Selection! Please select from 1 to 6 only.")
     return user_selection
 
-# Skeleton of the logic for calculator processing cycle.
+## Obtaining the Input Numbers from the user.
+def get_numbers_from_user():
+    # Store the user inputs in get_integer_function() and assign them to each variable to left.
+    num1 = get_integer_inputs("Enter your first number: ")
+    num2 = get_integer_inputs("Enter your second number: ")
+    return num1, num2
+
+# Below, the parameter message is the message provided as message from get_numbers_from_user().
+def get_integer_inputs(message):
+    value_as_string = input(message)
+    # If user input is not numeric ask for a valid entry until user provide one.
+    while not value_as_string.isnumeric():
+        print(f"Entry must be an integer. You entered {value_as_string}")
+        value_as_string = input(message)
+    return int(value_as_string)
+
+
+## Skeleton of the logic for calculator processing cycle.
 # Boolean flag, to determine weather to terminate the main processing loop/not.
 finished = False
 # Main processing loop of the calculator.
 while not finished:
     result = 0
+
     # Get the operation from the user.
     operator_choice = get_operation_choice()
 
     # Get the numbers from the user.
-    # Select the operation.
+    n1, n2 = get_numbers_from_user()
+
+    ## Select the operation.
+    # Each part of the if statement calls a different function.
+    # But they all store the value returned into the result variable.
+    if operator_choice == '1':
+        result = addition(n1,n2)
+    elif operator_choice == '2':
+        result = subtraction(n1,n2)
+    elif operator_choice == '3':
+        result = multiplication(n1,n2)
+    elif operator_choice == '4':
+        result = division(n1,n2)
+    elif operator_choice == '5':
+        result = modulus(n1,n2)
+    elif operator_choice == '6':
+        result = power_of(n1,n2)
     print(f"Result: {result}")
     # Determine if the user wants to continue/finished
     finished = check_if_the_user_has_finished()
 print("Thank you for using the Calculator App!")
 
-## Selecting the Operation
+
+
 
 
 
